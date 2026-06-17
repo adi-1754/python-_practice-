38a872f2831650e9328ee91ccaf5f26fdc8f53db
adi-1754 <adityakalbhor1754@gmail.com>
2026-06-17 18:55:22 +0530

    add some files and message

---
+++ OneDrive/Documents/python practice and study material/git_question/50 question_answer.py
@@ -0,0 +1,138 @@
+#1. 
+# a = int(input("Enter a first number"))
+# b = int(input("Enter a second number"))
+
+# sum = a + b 
+# print("Addition of two num : ", sum)
+
+
+# 2 . 
+# num1 = int(input("Enter a number 1 : "))
+# num2 = int(input("Enter a number 2 : "))
+
+# print("Addition : ",num1+num2)
+# print("Subtraction : ",num1-num2)
+# print("Multiplication : ",num1*num2)
+# print("Division : ",num1/num2)
+# print("Modules : ",num1 % num2)
+
+
+# 3. 
+# num = int(input("Enter a number "))
+# pow = num ** 2
+# print(f"Power of {num} is - {pow}")
+
+
+#  4 
+# base = float(input("Enter a base of triangle : "))
+# height = float(input("Enter a height of triangle : "))
+
+# area = (base * height) / 2 
+# print("Area of triangle :  ",area)
+
+
+# # 5
+# length = float(input("Enter a length of rectangle : "))
+# width = float(input("Enter a width of rectangle : "))
+# area = length * width 
+# print("Area of rectangle : ",area)
+
+
+# # 6 
+# num = int(input("Enter a number : "))
+# if num % 2 == 0 :
+#     print("Even ")
+# else:
+#     print("odd")     
+
+
+# # 7
+# num = int(input("Enter a number : "))
+# if num < 0 :
+#     print("The number is Negative ")
+# elif num > 0 :
+#     print("The number is Positive ")   
+# else:
+#     print("The number is Zero ")     
+
+
+# # 8
+# Marks = float(input("Enter your Marks : "))
+# if Marks >= 90 :
+#     print("Grade A ")
+# elif Marks >= 80: 
+#     print("Grade B ")
+# elif Marks >= 70:
+#     print("Grade C ")
+# elif Marks >= 60:
+#     print("Grade D ")
+# elif Marks < 60:
+#     print("Grade F ")
+# else: 
+#     print("Enter a valid number")                    
+
+
+# # 9
+# num1 = 100 
+# num2 = 20
+# num3 = 40 
+
+# if num1 > num2 and num1 > num3 : 
+#     print("num1 is Greater ")
+# elif num2 > num1 and num2 > num3 : 
+#     print("num 2 is Greater ")
+# else: 
+#     print("num 3 is Greater ")        
+
+
+# # 10
+# year = int(input("Enter a year : "))
+# if year % 4 == 0 and year != 100 :
+#     print("Leap year")
+# else:
+#     print("not leap year")    
+
+
+# # 11 
+# age = int(input("Enter your age :  "))
+# if age >= 18 : 
+#     print("You can Vote ")
+# else: 
+#     print("You can not Vote ")
+        
+
+# 12 
+# no_class_held = int(input("Enter a number of classes held : "))
+# no_class_attended = int(input("Enter a number of classes attended : "))
+# attendance_percentage = (no_class_attended / no_class_held) * 100
+# if attendance_percentage >= 75 :
+#     print("You are allowed to sit in the exam ",attendance_percentage)
+# else:
+#     print("You are not allowed to sit in the exam ",attendance_percentage)   
+
+
+
+# #  13
+# num = int(input("Enter a number: "))
+# if num % 3 == 0  and num % 5 == 0 : 
+#     print("FizzBuzz")
+# elif num % 3 == 0 :
+#     print("Fizz") 
+# elif num % 5 == 0 : 
+#     print("Buzz")       
+
+
+# # 14 
+# string_single = 'Aditya'
+# string_double = "Suhas "
+# string_triple = "'Kalbhor'"
+# print(f"single {string_single} double {string_double}  triple {string_triple}")
+
+
+string = input("Enter a string:  ")
+count = 0 
+for each_char in string:
+    if each_char == "a" or each_char == "A":
+        count += 1 
+print("A char in string ",count)        
+
