4805f4e7f74b9f185a6b30a5dbf0a0e2679a1d70
adi-1754 <adityakalbhor1754@gmail.com>
2026-07-03 15:07:26 +0530

    Add a question 1 and question 2

---
+++ demo.py
@@ -0,0 +1,20 @@
+# Question 1 Check if a number is positive, negative, or zero.
+ 
+# num = int(input("Enter a number: "))
+# if num < 0:
+#     print("The number is negative.")
+# elif num > 0:
+#     print("The number is positive.")
+# else:
+#     print("The number is zero.")
+
+
+# Question 2 Check if a number is even or odd.
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0: 
+#     print("The number is even.")
+# else: 
+#     print("The number is odd.")    
+
+
