24d92ef50a38a59022403ad16ccb5670f4cd3841
adi-1754 <adityakalbhor1754@gmail.com>
2026-07-03 18:50:16 +0530

    Add a question 1 and question 3

--- demo.py
+++ demo.py
@@ -9,6 +9,7 @@
 #     print("The number is zero.")
 
 
+
 # Question 2 Check if a number is even or odd.
 
 # num = int(input("Enter a number: "))
@@ -16,5 +17,20 @@
 #     print("The number is even.")
 # else: 
 #     print("The number is odd.")    
+
+
+
+# Question 3 Find the largest of two numbers.
+
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
+
+# if num1 >= num2 and num1 >= num3:
+#     print(f"The largest number is: {num1}")
+# elif num2 >= num1 and num2 >= num3:
+#     print(f"The largest number is: {num2}")
+# else:
+#     print(f"The largest number is: {num3}")
 
 
