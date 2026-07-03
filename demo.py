96e29070decbe6aa065c786fd56702ccb93e148e
adi-1754 <adityakalbhor1754@gmail.com>
2026-07-03 19:18:25 +0530

    add a question 4

--- demo.py
+++ demo.py
@@ -31,15 +31,16 @@
 #     print(f"The largest number is: {num2}")
 
 
+
 # Question 4 Find the largest of three numbers.
 
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# num3 = int(input("Enter the third number: "))
 
-if num1 >= num2 and num1 >= num3:
-    print(f"The largest number is: {num1}")
-elif num2 >= num1 and num2 >= num3:
-    print(f"The largest number is: {num2}")
-else:
-    print(f"The largest number is: {num3}")
+# if num1 >= num2 and num1 >= num3:
+#     print(f"The largest number is: {num1}")
+# elif num2 >= num1 and num2 >= num3:
+#     print(f"The largest number is: {num2}")
+# else:
+#     print(f"The largest number is: {num3}")
