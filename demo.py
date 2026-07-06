275b5ffa97c175df6929d3b6f55db1e3bf16f9f7
adi-1754 <adityakalbhor1754@gmail.com>
2026-07-06 19:16:40 +0530

    question 11.5

--- demo.py
+++ demo.py
@@ -118,9 +118,8 @@
 
 
 # Question 11 Check whether a number is a multiple of 3.
-num = int(input("Enter a number: "))
-if num % 3 == 0:
-    print(f"{num} is a multiple of 3 ")
-else : 
-    print(f"{num} is not a multiple of 3 ") 
-    
+# num = int(input("Enter a number: "))
+# if num % 3 == 0:
+#     print(f"{num} is a multiple of 3 ")
+# else : 
+#     print(f"{num} is not a multiple of 3 ") 
